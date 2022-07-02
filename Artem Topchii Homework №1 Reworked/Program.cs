de64ce319570b9e8e63a20bc81d0ab5dc93704ae
Artem Topchii <artem.t@vec-ua.com>
2022-07-02 17:21:21 +0300

    Homework 1 reworked - for review

--- Artem Topchii Homework №1 Reworked/Program.cs
+++ Artem Topchii Homework №1 Reworked/Program.cs
@@ -4,105 +4,120 @@ namespace Project
 {
     class Program
     {
-        static void ConvertHoursToMinutes()
+        static int ConvertHoursToMinutes(int numberOfHours)
         {
-            Console.WriteLine("Enter number of hours:");
-            int hours = Convert.ToInt32(Console.ReadLine());
-
-            int minutes = hours * 60;
-
-            Console.WriteLine(minutes + " min");
+            return numberOfHours * 60;
         }
 
-        static void SolveTheTask()
+        static int SolveTheTask(int a, int b)
         {
-            Console.WriteLine("Enter number A");
-            int a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter number B");
-            int b = Convert.ToInt32(Console.ReadLine());
-
-            int result = (5 * a + b * b) / (b - a);
-
-            Console.WriteLine(result);
+            return (5 * a + b * b) / (b - a);
         }
 
-        static void SwapTheContents()
+        static string SwapTheContents(string a, string b)
         {
-            Console.WriteLine("Enter value A");
-            string a = Console.ReadLine();
-            Console.WriteLine("Enter value B");
-            string b = Console.ReadLine();
-
             string temp = a;
             a = b;
             b = temp;
 
-            Console.WriteLine("Swap completed successfully" + Environment.NewLine + "Value A: " + a + Environment.NewLine + "Value B: " + b);
+            return "Value A: " + a + Environment.NewLine + "Value B: " +  b;
         }
 
-        static void DividingWithRemainder()
+        static (int, int) DividingWithRemainder(int a, int b)
         {
-            Console.WriteLine("Enter number A");
-            int a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter number B");
-            int b = Convert.ToInt32(Console.ReadLine());
-
-            int result = a / b;
-            int division_Remainder = a % b;
-
-            Console.WriteLine("Result: " + result + Environment.NewLine + "Division remainder: " + division_Remainder);
+            return (a / b, a % b);
         }
 
-        static void SolveTheEquation()
+        static float SolveTheEquation(float a, float b, float c)
         {
-            Console.WriteLine("Enter number A");
-            float a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter number B");
-            float b = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter number C");
-            float c = Convert.ToInt32(Console.ReadLine());
-
             //Linear equation: A * X + B = C
-            float x = (float)Math.Round((c - b) / a, 2);
-
-            Console.WriteLine("X = " + x);
+            return (float)Math.Round((c - b) / a, 2);
         }
 
-        static void SolveTheEquationOfStraightLine()
+        static (float, float) SolveTheEquationOfStraightLine(float x1, float x2, float y1, float y2)
         {
-            Console.WriteLine("Enter number X1");
-            float x1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter number Y1");
-            float y1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter number X2");
-            float x2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter number Y2");
-            float y2 = Convert.ToInt32(Console.ReadLine());
-
             float b = (y1 - y2) / (x1 - x2);
             float a = y2 - b * x2;
 
-            Console.WriteLine("Point A: X1 = " + x1 + " Y1 = " + y1 + Environment.NewLine +
-                              "Point B: X2 = " + x2 + " Y2 = " + y2 + Environment.NewLine +
-                              "Equation: y = " + b + "x + " + a);
+            return (a, b);
         }
 
-        static void CalculateSumOfDigits()
+        static (int, int) CalculateSumOfDigits(int n)
         {
-            Console.WriteLine("Enter two-digit number:");
-            int n = Convert.ToInt32(Console.ReadLine());
+            return (n / 10, n % 10);
+        }
 
-            Console.WriteLine(n / 10 + " + " + n % 10 + " = " + (n / 10 + n % 10));
-        }
-        static void Main(string[] args)
+        public static void Main()
         {
-            //ConvertHoursToMinutes();
-            //SolveTheTask();
-            //SwapTheContents();
-            //DividingWithRemainder();
-            //SolveTheEquation();
-            //SolveTheEquationOfStraightLine();
-            //CalculateSumOfDigits();
+         //CONVERT HOURS TO MINUTES
+
+            //int numberOfHours = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine(ConvertHoursToMinutes(numberOfHours) + " min");
+
+         //SOLVE THE TASK
+
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine(SolveTheTask(a, b));
+
+         //SWAP CONTENTS
+
+            //Console.WriteLine("Enter value A");
+            //string a = Console.ReadLine();
+            //Console.WriteLine("Enter value B");
+            //string b = Console.ReadLine();
+
+            //Console.WriteLine("Swap completed successfully" + Environment.NewLine + SwapTheContents(a, b));
+
+         //DIVIDING WITH REMAINDER
+
+            //Console.WriteLine("Enter number A");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter number B");
+            //int b = Convert.ToInt32(Console.ReadLine());
+
+            //(int result, int remainder) = DividingWithRemainder(a, b);
+
+            //Console.WriteLine("Result = " + result + Environment.NewLine +
+            //                  "Remainder = " + remainder);
+
+         //SOLVE THE EQUATION
+
+            //Console.WriteLine("Enter number A");
+            //float a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter number B");
+            //float b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter number C");
+            //float c = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("X = " + SolveTheEquation(a, b, c));
+
+         //SOLVE THE EQUATION OF STRAIGHT LINE
+
+            //Console.WriteLine("Enter number X1");
+            //float x1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter number Y1");
+            //float y1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter number X2");
+            //float x2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter number Y2");
+            //float y2 = Convert.ToInt32(Console.ReadLine());
+
+            //(float a, float b) = SolveTheEquationOfStraightLine(x1, x2, y1, y2);
+
+            //Console.WriteLine("Equation: y = " + b + "x + " + a);
+
+         //CALCULATE SUM OF DIGITS
+
+            //Console.WriteLine("Enter number two digits number");
+            //int n = Convert.ToInt32(Console.ReadLine());
+
+            //(int firstDigit, int secondDigit) = CalculateSumOfDigits(n);
+
+            //Console.WriteLine(firstDigit + " + " + secondDigit + " = " + (firstDigit + secondDigit));
         }
+
     }
 }
